924840a284fe5b767a752b5d480a06e43fa55e5d
kongjunyihao <xutianxiang47@gmail.com>
2024-07-17 22:21:06 -0700

    create webpage of SNMP

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,18 +1,50 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Dropdown from '@mui/joy/Dropdown';
+import Menu from '@mui/joy/Menu';
+import MenuButton from '@mui/joy/MenuButton';
+import MenuItem from '@mui/joy/MenuItem';
+import '@fontsource/inter';
 
-export default function SeerchPage(){
+import { Avatar, Container, createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme();
+
+export default function SearchPage() {
     const [ip, setIp] = useState("");
     const [mib, setMib] = useState("");
+    const [version, setVersion] = useState("");
 
-    const IPhandleer = () => {};
+    const handleIP = () => { };
+
+
 
     return (
-        <>
-            <h1>SWNMP Network Device Management System</h1>
-            <div>
-                <input type="text" />
-                <button>search</button>
-            </div>
-        </>
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="100%">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        width: "100%"
+                    }}
+                >
+                    <Typography component="h3" variant="h3">
+                        SNMP Network Device Management System
+                    </Typography>
+                    <Box component="form" noValidate onSubmit={handleIP} sx={{ mt: 3 }}>
+                        <TextField id="outlined-search" label="Search field" type="search" />
+                        <TextField id="outlined-search" label="Search field" type="search" />
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
     );
 }
